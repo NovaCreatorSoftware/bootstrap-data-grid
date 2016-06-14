5d53da3166ebd9a1093f48276bec9aaf20b066f2
Cristian Mihai Manole <cristimanole@gmail.com>
2016-06-14 03:53:41 +0300

    .

---
+++ dist/js/novacreator.bootstrap.datagrid.fa.js
@@ -0,0 +1,19 @@
+/*! 
+ * Nova Creator Bootstrap Datagrid v1.0.0 - 06/14/2016
+ * Copyright (c) 2015-2016 Nova Creator Software (https://github.com/NovaCreatorSoftware/bootstrap-data-grid)
+ * Licensed under MIT http://www.opensource.org/licenses/MIT
+ */
+;(function ($, window, undefined)
+{
+    /*jshint validthis: true */
+    "use strict";
+
+/*$.extend($.fn.tablear.Constructor.defaults.css, {
+    icon: "icon fa",
+    iconColumns: "fa-th-list",
+    iconUp: "fa-sort-asc",
+    iconDown: "fa-sort-desc",
+    iconRefresh: "fa-refresh",
+    iconSearch: "fa-search"
+});*/
+})(jQuery, window);

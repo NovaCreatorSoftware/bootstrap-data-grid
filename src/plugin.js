bac68ff050787af6b6bbe569dbb0fc4a009bd814
Cristian Mihai Manole <cristimanole@gmail.com>
2016-07-13 15:26:46 +0300

    .

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,7 @@
 // GRID PLUGIN DEFINITION
 // =====================
 var old = $.fn.tablear;
+
 $.fn.tablear = function(option) {
     var args = Array.prototype.slice.call(arguments, 1);
     var returnValue = null;
